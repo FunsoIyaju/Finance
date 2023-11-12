bbca15c49a110f261d49a4aaa924016373b7a2cf
FunsoIyaju <stepheniyaju@gmail.com>
2023-11-12 03:59:20 +0100

    Make changes to the code to make the stock price global and used by all tabs

--- FinancialApp/financialAnalyses.py
+++ FinancialApp/financialAnalyses.py
@@ -2,6 +2,7 @@
 import numpy as np
 import pandas as pd
 import plotly.graph_objects as go
+#import plotly.express as px
 from plotly.subplots import make_subplots
 #from datetime import datetime, timedelta
 import yfinance as yf
